fce76fff027f1e3577200d4aadf6a2dd9e901423
Minh Nguyen <minh.nguyen@hodfords.com>
2024-04-23 21:51:26 +0700

    feat: Prevent send trading to funding BTC when cant earning

--- src/okx-api/services/okx-api-earn.service.ts
+++ src/okx-api/services/okx-api-earn.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { OKX_REST_PRIVATE_CLIENT } from '~core/constants/okx.constant';
+import { ASSETS } from '~core/constants/crypto-code.constant';
+import { OKX_MIN_SAVING_BTC_ETH, OKX_REST_PRIVATE_CLIENT } from '~core/constants/okx.constant';
 
 @Injectable()
 export class OkxApiEarnService {
@@ -24,20 +25,18 @@ export class OkxApiEarnService {
         try {
             const balance = await OKX_REST_PRIVATE_CLIENT.getBalance(asset);
             const amount = balance[0].details[0]?.availBal;
-            if (amount) {
+            if (
+                (asset === ASSETS.CRYPTO.BTC || asset === ASSETS.CRYPTO.ETH) &&
+                parseFloat(amount) >= OKX_MIN_SAVING_BTC_ETH
+            ) {
                 await OKX_REST_PRIVATE_CLIENT.fundsTransfer({
                     ccy: asset,
-                    amt: amount.substring(0, 10),
+                    amt: amount,
                     from: '18',
                     to: '6',
                     type: '0'
                 });
-                await OKX_REST_PRIVATE_CLIENT.savingsPurchaseRedemption(
-                    asset,
-                    amount.substring(0, 10),
-                    'purchase',
-                    '0.01'
-                );
+                await OKX_REST_PRIVATE_CLIENT.savingsPurchaseRedemption(asset, amount, 'purchase', '0.01');
             }
         } catch (error) {
             console.error('OKX purchaseMaxToSaving error:', error);
